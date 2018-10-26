86be3cf4edca6ff4212178ac51058f7f9f9a4bf6
josefina <31647229+Raseration@users.noreply.github.com>
2018-10-26 15:03:17 +0200

    All the stuff

---
+++ Assets/FireGun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class FireGun : MonoBehaviour {
+
+    private GameObject bullet;
+    private float bulletSpeed = 1000f;
+    private float bulletLife = 5f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	public void FireBullet()
+    {
+        bullet = transform.Find("Bullet").gameObject;
+        bullet.SetActive(false);
+        GameObject bulletClone = Instantiate(bullet, bullet.transform.position, bullet.transform.rotation) as GameObject;
+        bulletClone.SetActive(true);
+        Rigidbody rb = bulletClone.GetComponent<Rigidbody>();
+        rb.AddForce(-bullet.transform.forward * bulletSpeed);
+        Destroy(bulletClone, bulletLife);
+    }
+}
